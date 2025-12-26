a65f70e3b98506c31432e1a057da95f3f8432bb4
Fantom <arthur.morelon@gmail.com>
2025-12-26 14:57:13 +0100

    fix create card route

--- start/routes/cards.ts
+++ start/routes/cards.ts
@@ -1,13 +1,16 @@
 import router from '@adonisjs/core/services/router'
 import {middleware} from "#start/kernel";
+import {CreateCardService} from "#app/modules/cards/application/contracts/create.card.service";
 
-const ListCardsController = () => import('../../app/modules/cards/infrastructure/controllers/get.cards.controller')
-const QuizzController = () => import('../../app/modules/quizz/cards/infrastructure/controllers/quizz.controller')
-const AnswerCardController = () => import('../../app/modules/cards/infrastructure/controllers/answer.card.controller')
+const ListCardsController = () => import('#app/modules/cards/infrastructure/controllers/get.cards.controller')
+const QuizzController = () => import('#app/modules/quizz/cards/infrastructure/controllers/quizz.controller')
+const AnswerCardController = () => import('#app/modules/cards/infrastructure/controllers/answer.card.controller')
+const CreateCardServicen = () => import('#app/modules/cards/infrastructure/controllers/create.card.controller')
 
 router.group(() => {
   router.get('/', [ListCardsController, 'handle'])
   router.get('/quizz', [QuizzController, 'handle'])
+  router.post('/', [CreateCardServicen, 'handle'])
   router.patch('/:id/answer', [AnswerCardController, 'handle']).where('id', router.matchers.uuid())
 
 }).middleware(middleware.authentification())
